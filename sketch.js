89412b5c6837eadd7b01c464a8995c317f2283a2
Yash <guptayash0609@gmail.com>
2021-09-22 15:05:39 +0530

    1st

---
+++ sketch.js
@@ -0,0 +1,164 @@
+var gameState = 1;
+var button1,button2,button3;
+var sound = new Audio("sound.mp3");
+var packageBody,ground,dropPoint,dropPointIMG,boy,boyIMG;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+
+function preload(){
+   bg = loadImage("images/bg3.jpg");
+   logo = loadImage("images/logo.jpg");
+   boyIMG = loadImage("images/boy.png");
+   packageIMG = loadImage("images/package.png");
+   dropPointIMG = loadImage("images/dropPoint.png");
+   helicopter1 = loadAnimation("images/helicopter1.png");
+   helicopter2 = loadAnimation("images/helicopter1.png","images/helicopter2.png","images/helicopter3.png","images/helicopter4.png");
+}
+
+function setup() {
+    createCanvas(1100, 710);
+
+    groundSprite=createSprite(width/2, height-5, width,10);
+    groundSprite.shapeColor=color(255);
+    groundSprite.visible = false;
+
+    dropPoint = createSprite(870,630,50,20);
+    dropPoint.addImage(dropPointIMG);
+    dropPoint.scale = 0.48;
+    dropPoint.debug = false;
+    dropPoint.setCollider("obb",0,80,350,120);
+
+    boy = createSprite(1010,590,20,20);
+    boy.addImage(boyIMG);
+    boy.scale = 0.4;
+
+    packageSprite = createSprite(200,220,80,80);
+    packageSprite.addImage(packageIMG);
+    packageSprite.scale = 0.25;
+
+    helicopterSprite=createSprite(200,220, 10,10);
+    helicopterSprite.addAnimation("helicopter",helicopter2);
+    helicopterSprite.scale=0.75;
+    sound.play();
+
+    button1 = createButton('🡸');
+    button1.position(780,50);
+    button1.style('font-size','28px');
+    button1.style('width', '55px');
+    button1.style('height', '42px');
+    button1.style('background',  rgb(255, 185, 207));
+	
+    button2 = createButton('🡺');
+    button2.position(880,50);
+    button2.style('font-size','28px');
+    button2.style('width', '55px');
+    button2.style('height', '42px');
+    button2.style('background', rgb(255, 185, 207));
+    button3 = createButton('DROP THE PACKAGE');
+    button3.position(750,100);
+    button3.style('font-size','20px');
+    button3.style('width', '220px');
+    button3.style('height', '40px');
+    button3.style('background', rgb(44, 255, 59));
+
+}
+
+function draw() {
+  background(bg);
+ 
+  packageSprite.collide(groundSprite);
+  image(logo,300,30,400,100)
+
+  if(gameState === 1){
+     packageSprite.x = helicopterSprite.x;
+     keyPressed();
+     key1();
+     key2();
+     key3();
+     button1.mousePressed(() => {
+       helicopterSprite.x = helicopterSprite.x-35;
+       helicopterSprite.addAnimation("helicopter",helicopter2);
+     })
+     button2.mousePressed(() => {
+        helicopterSprite.x = helicopterSprite.x+35;
+        helicopterSprite.addAnimation("helicopter",helicopter2);
+     })
+  }
+
+  if(gameState === 2){
+	   packageSprite.velocityY = packageSprite.velocityY+2;
+	   restart = createButton('RESTART');
+      restart.position(400,480);
+      restart.style('font-size','25px');
+      restart.style('width', '180px');
+      restart.style('height', '32px');
+      restart.style('background', rgb(255, 185, 207));
+      restart.mousePressed(()=>{
+	      location.reload();
+      })
+
+	if(packageSprite.isTouching(dropPoint)){
+	   packageSprite.collide(dropPoint);
+	   gameState = 3;
+	}
+
+	button1.mousePressed(() => {
+	   helicopterSprite.x = helicopterSprite.x-0;
+	})
+
+	button2.mousePressed(() => {
+	   helicopterSprite.x = helicopterSprite.x+0;
+	})
+
+   }	 
+
+  if(gameState === 3){
+	   correct();
+	   restart = createButton('RESTART');
+      restart.position(400,480);
+      restart.style('font-size','25px');
+      restart.style('width', '180px');
+      restart.style('height', '32px');
+      restart.style('background', rgb(255, 185, 207));
+      restart.mousePressed(()=>{
+	   location.reload();
+      })
+  }
+
+  drawSprites();
+}
+
+function keyPressed() {
+  if(keyCode === DOWN_ARROW) {
+    gameState = 2;   
+  }
+}
+
+function key1() {
+	if(keyIsDown(LEFT_ARROW)){
+	   helicopterSprite.x = helicopterSprite.x-10;
+	   helicopterSprite.addAnimation("helicopter",helicopter2);
+	}
+}
+
+function key2() {
+    if(keyIsDown(RIGHT_ARROW)){
+       helicopterSprite.x = helicopterSprite.x+10;
+       helicopterSprite.addAnimation("helicopter",helicopter2);	   
+       }
+}
+
+function key3(){
+    button3.mousePressed(() => {
+       gameState = 2;
+    })
+}
+
+function correct(){
+   strokeWeight(4);
+   stroke("yellow");
+   fill("orangered");
+   textSize(55);
+   textFont("MV Boli");
+   text("Nice!!! You Dropped The Parcel",130,350);
+   text("At correct Place",300,420);
+}
